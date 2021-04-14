c14e26171d0f1891a8253912502052892af46fdb
finn <finnandshadow@gmail.com>
2021-04-14 17:11:58 -0400

    Basic, really bad "Platformer" now exists

--- Src/Playground/PhysicsPlayground.cc
+++ Src/Playground/PhysicsPlayground.cc
@@ -1,6 +1,7 @@
 #include "Flux/Debug.hh"
 #include "Flux/Physics.hh"
 #include "FluxArc/FluxArc.hh"
+#include "glm/detail/func_common.hpp"
 #include <cstdio>
 #include <cstdlib>
 #define GLM_FORCE_CTOR_INIT
@@ -18,7 +19,7 @@
 
 Flux::ECSCtx ctx;
 
-Flux::EntityRef quad;
+Flux::EntityRef player;
 Flux::EntityRef camera;
 Flux::EntityRef cube_stick;
 
@@ -31,7 +32,7 @@ Flux::Resources::ResourceRef<Flux::Renderer::MaterialRes> mat_res;
 
 std::vector<Flux::EntityRef> meshes;
 
-Flux::EntityRef shapes[3];
+// Flux::EntityRef shapes[3];
 
 void init(int argc, char** argv) {
     
@@ -39,11 +40,12 @@ void init(int argc, char** argv) {
 
     int c = 0;
 
-    cube_stick = ctx.createEntity();
-    Flux::Transform::giveTransform(cube_stick);
+    // cube_stick = ctx.createEntity();
+    // Flux::Transform::giveTransform(cube_stick);
 
-    auto loader = Flux::Resources::deserialize("assets/Shapes.farc");
+    auto loader = Flux::Resources::deserialize("assets/Platforms.farc");
     auto ens = loader->addToECS(&ctx);
+    Flux::Transform::rotate(ens[0], glm::vec3(0, 1, 0), glm::radians(90.0f));
 
     int pos = -4;
     for (auto i : ens)
@@ -52,17 +54,25 @@ void init(int argc, char** argv) {
         {
             // Flux::Physics::giveBoundingBox(i);
             Flux::Physics::giveConvexCollider(i);
-            shapes[c] = i;
+            // shapes[c] = i;
             c ++;
             meshes.push_back(i);
-            Flux::Transform::removeParent(i);
-            Flux::Transform::setParent(i, cube_stick);
-            Flux::Transform::translate(i, glm::vec3(pos, 0, 0));
+            // Flux::Transform::removeParent(i);
+            // Flux::Transform::setParent(i, cube_stick);
+            // Flux::Transform::translate(i, glm::vec3(pos, 0, 0));
+            auto entity = ctx.createEntity();
+            i.getComponent<Flux::Physics::BoundingCom>()->box->setDisplayEntity(entity);
             pos += 4;
         }
         else
         {
-            ctx.destroyEntity(i);
+            // if (i.hasComponent<Flux::NameCom>())
+            // {
+            //     if (i.getComponent<Flux::NameCom>()->name == "Player_Cube.003")
+            //     {
+            //         ctx.destroyEntity(i);
+            //     }
+            // }
         }
     }
 
@@ -96,6 +106,14 @@ void init(int argc, char** argv) {
 
     Flux::Renderer::addPointLight(camera, 80, glm::vec3(1, 1, 1));
 
+    player = ctx.getNamedEntity("Player");
+    Flux::Transform::removeParent(player);
+    Flux::Transform::setParent(camera, player);
+
+    // auto pcc = (Flux::Physics::ConvexCollider)player.getComponent<Flux::Physics::ColliderCom>()->collider;
+
+    Flux::Transform::translate(player, glm::vec3(0, 3, 0));
+
     Flux::GLRenderer::addGLRenderer(&ctx);
     LOG_INFO("Added GL Renderer");
     Flux::Transform::addTransformSystems(&ctx);
@@ -104,58 +122,75 @@ void init(int argc, char** argv) {
     Flux::Debug::enableDebugDraw(&ctx);
 }
 
+glm::vec3 velocity(0, -8, 0);
+bool on_ground = false;
+
 void loop(float delta)
 {
     // Flux::Transform::rotate(cube_stick, glm::vec3(0, 1, 0), 0.5 * delta);
-
+    // glm::vec3 velocity(0, 0, 0);
     if (Flux::Input::isKeyPressed(FLUX_KEY_LEFT))
     {
-        Flux::Transform::translate(shapes[0], glm::vec3(-5 * delta, 0, 0));
+        // Flux::Physics::move(player, glm::vec3(-5 * delta, -8 * delta, 0));
+        velocity.x = -5;
+    }
+    else if (Flux::Input::isKeyPressed(FLUX_KEY_RIGHT))
+    {
+        // Flux::Physics::move(player, glm::vec3(5 * delta, -8 * delta, 0));
+        velocity.x = 5;
+    }
+    else
+    {
+        velocity.x = 0;
     }
 
-    if (Flux::Input::isKeyPressed(FLUX_KEY_RIGHT))
+    if (Flux::Input::isKeyPressed(FLUX_KEY_UP) && on_ground)
     {
-        Flux::Transform::translate(shapes[0], glm::vec3(5 * delta, 0, 0));
+        velocity.y = 8;
     }
 
-    if (Flux::Input::isKeyPressed(FLUX_KEY_UP))
-    {
-        Flux::Transform::translate(shapes[0], glm::vec3(0, 5 * delta, 0));
-    }
+    // velocity.y -= 8;
+    // velocity.y = glm::min(-8.0f, velocity.y);
 
-    if (Flux::Input::isKeyPressed(FLUX_KEY_DOWN))
+    auto out = Flux::Physics::move(player, velocity * delta);
+    if (out.size() > 0)
     {
-        Flux::Transform::translate(shapes[0], glm::vec3(0, -5 * delta, 0));
-    }
-
-    {
-        // auto cc = shapes[0].getComponent<Flux::Physics::ColliderCom>();
-        auto collisions = Flux::Physics::getCollisions(shapes[0]);
-        if (collisions.size() > 0)
+        // Check collision vector
+        if (glm::dot(out[0].normal, glm::vec3(0, -1, 0)) < 0)
         {
-            Flux::Renderer::setUniform(shapes[0].getComponent<Flux::Renderer::MeshCom>()->mat_resource, "color", glm::vec3(1, 0, 0));
-            // LOG_INFO("Hitting");
-
-            // Show collision information
-            auto data = collisions[0];
-            auto endpoint = (glm::vec3(0, 5, 0) + data.normal) * data.depth;
-            // Flux::Debug::drawLine(glm::vec3(0, 5, 0), endpoint, Flux::Debug::Colors::Purple);
-            // Flux::Debug::drawPoint(endpoint, 0.15, Flux::Debug::Colors::Purple);
-
-            // if (Flux::Input::isKeyPressed(FLUX_KEY_SPACE))
-            // {
-            Flux::Transform::translate(shapes[0], data.normal * data.depth);
-            // }
+            // I think we're on the ground
+            // Don't apply gravity
+            on_ground = true;
         }
         else
         {
-            Flux::Renderer::setUniform(shapes[0].getComponent<Flux::Renderer::MeshCom>()->mat_resource, "color", glm::vec3(0, 1, 0));
-            // LOG_INFO("Not");
+            // We hit something that isn't ground
+            // if (velocity.y > -8)
+            // {
+                // velocity.y -= 8 * delta;
+            // }
+            // else
+            // {
+                velocity.y = -8;
+            // }
+
+            on_ground = false;
         }
     }
+    else
+    {
+        if (velocity.y > -8)
+        {
+            velocity.y -= 8 * delta;
+        }
+        else
+        {
+            velocity.y = -8;
+        }
 
-    // Flux::Debug::drawPoint(glm::vec3(0, 1.5, 0), 1, Flux::Debug::Colors::Red, true);
-    // Flux::Debug::drawLine(glm::vec3(10, 2, 0), glm::vec3(-10, 2, 0), Flux::Debug::Colors::Purple);
+        on_ground = false;
+    }
+    // std::cout << out.size() << "\n";
 
     ctx.runSystems(delta);
     Flux::Debug::run(delta);
